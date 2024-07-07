e79b399400f18f86eb389dc619d436cb3cc7dece
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-07-07 02:26:07 +0100

    userController, testing api get User By ID

--- controllers/userController.js
+++ controllers/userController.js
@@ -23,8 +23,25 @@ async function getUsers(req, res) {
     }
 }
 
+// Get a user by ID
+async function getUserById(req, res) {
+    const userId = req.params.id;
+    try {
+        const user = await User.findByPk(userId);
+        if (!user) {
+            res.status(404).json({ error: "User not found" });
+        } else {
+            res.status(200).json(user);
+        }
+    } catch (error) {
+        console.error("Error fetching users.", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+}
+
 
 module.exports = {
     createUser,
-    getUsers
+    getUsers,
+    getUserById
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,5 +5,6 @@ const userController = require("../controllers/userController");
 //Define API routes for the User resource
 router.post('/user', userController.createUser);
 router.get('/user', userController.getUsers);
+router.get('/user/:id', userController.getUserById);
 
 module.exports = router;
