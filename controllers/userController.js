ddcb350608702fcdd803af6ae8612de771568548
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-07-07 02:21:13 +0100

    userController, testing api get All Users

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,6 +12,19 @@ async function createUser(req, res) {
     }
 }
 
+// Get all users
+async function getUsers(req, res) {
+    try {
+        const users = await User.findAll();
+        res.status(200).json(users);
+    } catch (error) {
+        console.error("Error fetching users.", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+}
+
+
 module.exports = {
-    createUser
+    createUser,
+    getUsers
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,5 +4,6 @@ const userController = require("../controllers/userController");
 
 //Define API routes for the User resource
 router.post('/user', userController.createUser);
+router.get('/user', userController.getUsers);
 
 module.exports = router;
