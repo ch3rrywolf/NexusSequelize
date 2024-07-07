32454eec80011984a7da5f1b1d20654e5e065214
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-07-07 05:59:13 +0100

    Getters, Setters, Virtual fields testing..

--- models/user.js
+++ models/user.js
@@ -15,10 +15,32 @@ const User = sequelize.define("User", {
         type: DataTypes.STRING,
         unique: true,
         allowNull: false,
-        validate: {
-            isEmail: true,
+        set(value) {
+            this.setDataValue("email", value.toLowerCase());
+        }
+        // validate: {
+        //     isEmail: true,
+        // }
+    },
+    fullName: {
+        type: DataTypes.VIRTUAL,
+        get() {
+            const firstName = this.getDataVAlue("firstName");
+            const lastName = this.getDataVAlue("lastName");
+            return `${firstName} ${lastName}`;
         }
     }
 });
+
+(async () => {
+    await sequelize.sync();
+
+    const user = await User.create({
+        firstName: "wolf",
+        lastName: "cherry",
+        email: "gh@hj.com"
+    });
+    console.log(user.email);
+})();
 
 module.exports = User;
