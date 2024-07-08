f5f2c67294e4e0dc57ebf867f2d1adc63d51563a
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-07-08 16:34:18 +0100

    fields validation with Joi package

--- stage2/Routes/Auth.routes.js
+++ stage2/Routes/Auth.routes.js
@@ -3,31 +3,31 @@ const router = express.Router();
 const createError = require("http-errors");
 const { Users } = require('../models');
 const bcrypt = require("bcrypt");
+const { authSchema } = require("../helpers/authValidation_schema");
 
 // Registration API
 // POST /register endpoint
 router.post("/register", async (req, res, next) => {
     try {
-        const { username, email, password } = req.body;
-        if (!username || !email || !password) throw createError.BadRequest('Username, email, and password are required.');
+        // const { username, email, password } = req.body;
+        // if (!username || !email || !password) throw createError.BadRequest('Username, email, and password are required.');
+
+        const result = await authSchema.validateAsync(req.body);
 
         if (!Users) throw createError.InternalServerError('Users model is not defined.');
 
-        const existingUser = await Users.findOne({ where: { email } });
-        if (existingUser) throw createError.Conflict(`${email} is already registered.`);
+        const existingUser = await Users.findOne({ where: { email: result.email } });
+        if (existingUser) throw createError.Conflict(`${result.email} is already registered.`);
 
-        const hashpassword = await bcrypt.hash(password, 10);
+        const hashpassword = await bcrypt.hash(result.password, 10);
 
-        const user = await Users.create({
-            username: username,
-            email: email,
-            password: hashpassword,
-        }).then(user => {
+        const user = await Users.create(result).then(user => {
             user.password = undefined;
             res.status(200).send(user);
         });
 
     } catch (error) {
+        if(error.isJoi === true ) error.status = 422;
         next(error); 
     }
 });
